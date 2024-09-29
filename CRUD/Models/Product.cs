14cd47cf16ffed6c0293d310e0f93043e9ea91a3
castilloglenn <castilloglenn@ymail.com>
2024-09-29 23:40:58 +0800

    feat: add product model

---
+++ CRUD/Models/Product.cs
@@ -0,0 +1,9 @@
+namespace CRUD.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}

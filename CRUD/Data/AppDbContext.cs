3013ac8e12aecadf97b492eb8ffae9a08f98e4b4
castilloglenn <castilloglenn@ymail.com>
2024-09-30 10:07:54 +0800

    feat: app db context

---
+++ CRUD/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using CRUD.Models;
+
+namespace CRUD.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
